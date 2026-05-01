235f3336226b29bf2bdd939bcc1837ad2afb3afd
Anna <BelovaAP1@transport.mos.ru>
2026-05-01 17:39:10 +0300

    splash and icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,61 @@
 #include <QApplication>
 #include <QThread>
+#include <QSplashScreen>
 #include "mainwindow.h"
 #include "simulation.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QString iconPath = ":/icon.png";
+    QString imagePath = ":/splash.png";
+    QPixmap pixmap(imagePath);
 
+    if (pixmap.isNull())
+    {
+        qDebug() << "ERROR: Failed to load splash image!";
+    }
+
+    QPixmap originalPixmap(iconPath);
+
+    if (originalPixmap.isNull())
+    {
+        qDebug() << "ERROR: Failed to load original image!";
+    }
+
+    QPixmap scaledIcon = originalPixmap.scaled(
+        256, 256,
+        Qt::KeepAspectRatio,
+        Qt::SmoothTransformation
+    );
+
+    QIcon icon(scaledIcon);
+
+    if(icon.availableSizes().isEmpty())
+    {
+        qDebug()<<"ERROR: Fauld to load icon!";
+    }
+
+    a.setWindowIcon(icon);
+
+    int desiredWidth = 800;
+    int desiredHeight = 800;
+
+    QPixmap scaledPixmap = pixmap.scaled(
+        desiredWidth,
+        desiredHeight,
+        Qt::KeepAspectRatio,
+        Qt::SmoothTransformation
+    );
+
+    QSplashScreen splash(scaledPixmap);
+    splash.show();
+    splash.repaint();
+    splash.raise();
+    a.processEvents();
+    QTime dieTime = QTime::currentTime().addSecs(2);
+        while (QTime::currentTime() < dieTime) {
+            a.processEvents(QEventLoop::AllEvents, 100);
+        }
     // 1. Создаем поток-"двигатель"
     QThread* simThread = new QThread();
 
@@ -25,7 +75,7 @@ int main(int argc, char *argv[]) {
     MainWindow w(sim);
     w.setWindowTitle("Conference Party - SUE Moscow Metro R&D");
     w.show();
-
+    splash.finish(&w);
     // 6. ЗАПУСК
     simThread->start();
 
